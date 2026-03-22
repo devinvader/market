40143735c255ec0813393cd5a706c6e2aaad33d9
devinvader <245618441+devinvader@users.noreply.github.com>
2026-03-22 21:33:53 +0300

    Маппер обновил

--- src/main/java/ru/devinvader/market/mapper/OrderItemMapper.java
+++ src/main/java/ru/devinvader/market/mapper/OrderItemMapper.java
@@ -4,11 +4,13 @@ import org.springframework.stereotype.Component;
 import ru.devinvader.market.domain.CartItem;
 import ru.devinvader.market.domain.Order;
 import ru.devinvader.market.domain.OrderItem;
+import ru.devinvader.market.domain.OrderItemId;
 
 @Component
 public class OrderItemMapper {
 
-    public OrderItem fromDto(Order order, CartItem item) {
-        return new OrderItem(order, item.getItem(), item.getCount());
+    public OrderItem fromDto(Order order, CartItem cartItem) {
+        OrderItemId id = new OrderItemId(order.getId(), cartItem.getId().getItemId());
+        return new OrderItem(id, cartItem.getCount());
     }
 }
