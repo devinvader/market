902c9831ac4f8f7b49fc0dbefd996033c916115b
devinvader <245618441+devinvader@users.noreply.github.com>
2026-03-25 15:00:43 +0300

    Обновил тест для запуска контекста

--- src/test/java/ru/devinvader/market/MarketApplicationTests.java
+++ src/test/java/ru/devinvader/market/MarketApplicationTests.java
@@ -2,8 +2,10 @@ package ru.devinvader.market;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import({TestcontainersConfiguration.class})
 class MarketApplicationTests {
 
 	@Test
